651ffa35792032c94679949439bd4aafb35b998c
REclipsent <wmyellowpig@gmail.com>
2024-07-17 09:28:09 -0500

    Add league champion rolling

--- functions.py
+++ functions.py
@@ -29,3 +29,34 @@ def roll_agent(role: str = None):
         return choice(json_data)["Name"]
     except ValueError:
         return choice(json_data)["Name"]
+
+
+def roll_champion(role: str = None):
+    with open('./champions.json', 'r') as f:
+        json_data = load(f)
+
+    if role is None:
+        return choice(json_data)["Name"]
+
+    try:
+        role = role[0].lower()
+
+        match role:
+            case "t":
+                role = "TOP"
+            case "j":
+                role = "JUNGLE"
+            case "m":
+                role = "MID"
+            case "b" | "a":
+                role = "BOT"
+            case "s":
+                role = "SUPPORT"
+            case _:
+                raise ValueError(f"Invalid role: {role}")
+
+        return choice(list(filter(lambda champion: role in champion["Role"], json_data)))["Name"]
+    except IndexError:
+        return choice(json_data)["Name"]
+    except ValueError:
+        return choice(json_data)["Name"]
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import random
 import json
 from my_client import EclipsedGroovy
-from functions import roll_agent
+from functions import roll_agent, roll_champion
 import discord
 
 intents = discord.Intents.default()
@@ -37,6 +37,17 @@ async def agent(ctx: discord.Interaction, agent_role: str = None):
     else:
         print(f'{ctx.user} has rolled {rolled_agent} in the agent roll with {agent_role}')
     await ctx.response.send_message(f'You Rolled {rolled_agent}')
+
+
+@client.tree.command()
+async def champion(ctx: discord.Interaction, champion_role: str = None):
+    rolled_champion = roll_champion(champion_role)
+    if champion_role is None:
+        print(f'{ctx.user} has rolled {rolled_champion} in the champion roll')
+    else:
+        print(f'{ctx.user} has rolled {rolled_champion} in the champion roll with {champion_role}')
+    await ctx.response.send_message(f'You Rolled {rolled_champion}')
+
 
 if __name__ == '__main__':
     client.run(discord_key)
